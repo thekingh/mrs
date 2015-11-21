4f78ddf980f10111688af0c752325df060d171f0
ctgowrie <ctgowrie@gmail.com>
2015-11-21 13:30:25 -0500

    toString for grid and grid constructor

--- src/Grid.java
+++ src/Grid.java
@@ -7,18 +7,51 @@
 package src;
 
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class Grid {
 
 	// map from node id to coordinate
-	private Map<Integer, Coordinate> nodes;  
-	private Object[][] grid;
-	private int h, w;
+	private final List<GridObject> nodes;  
+	private final Object[][] grid;
+	private final int w;
+	private final int h;
 
-	public Grid() {
-		h = 0;
-		w = 0;
+	public Grid(List<GridObject> nodes, List<GridObject> edges, int w, int h) {
+		this.nodes = nodes;
+		this.w = w;
+		this.h = h;
+		this.grid = new Object[w][h];
+
+		List<GridObject> merged = new ArrayList<GridObject>();
+		merged.addAll(nodes);
+		merged.addAll(edges);
+
+		// place all objects at proper place in grid based on cartesian location
+		for (GridObject o : merged) {
+			Coordinate coord = o.c();
+			this.grid[coord.x()][coord.y()] = o.o();
+		}
+	}
+
+	public String toString() {
+		String str = "";
+		for (int i = 0; i < w; i++) {
+			// NEEDSWORK: do we want positive y down
+			for (int j = h-1; j >= 0; j++) {
+				Object o = grid[i][j];
+				if (o == null) {
+					str += " ";
+				} else {
+					str += o.toString();
+				}
+			}
+			str += "/n";
+		}
+
+		return str;
 	}
 
 }
--- src/GridObject.java
+++ src/GridObject.java
@@ -4,12 +4,20 @@ public class GridObject {
 	// if we need this then we could have edge and node and one be null
 	// then two constructors for it one with edge one with node
     //
-    public final Object o;
-    public final Coordinate c;
+    private final Object o;
+    private final Coordinate c;
     
     public GridObject(Object o, Coordinate c) {
         this.o = o;
         this.c = c;
+    }
+
+    public Object o() {
+    	return o;
+    }
+
+    public Coordinate c() {
+    	return c;
     }
 
     public String toString() {
