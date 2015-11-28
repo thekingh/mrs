126d6cda2d37835e3af77b7c2a20798539861119
atong01 <alexanderytong@gmail.com>
2015-11-28 16:14:50 -0500

    added distance metrics to coordinate class

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,5 +1,7 @@
 package src;
 
+import Math;
+
 public class Coordinate {
 	private final Pair<Integer, Integer> coord;
 
@@ -22,4 +24,50 @@ public class Coordinate {
 	public String toString() {
 		return String.format("(%d, %d)", x(), y());
 	}
+
+    /**
+     * Calculates a new coordinate relative to an input coordinate
+     * Note that coordinate system is x pos is right and y pos is up
+     *
+     * @param curr      The current coordinate in (x,y) representation
+     * @param dir       The relative direction of the new point relative to the
+     *                  current point note that dir is in the set {0,1,2,3},
+     *                  where 0=North, 1=East, 2=South, 3=West.
+     * @param ext       The distance between the new point and the current point
+     *                  i.e. ext(ension) of 0 is adjacent
+     * @return          The new absolute coordinate
+     */
+    public Coordinate calcRelativeLoc(int dir, int ext) {
+        x = x();
+        y = y();
+        switch(dir) {
+            case 0: //North
+                y += 1 + ext;
+                break;
+            case 1: //East
+                x += 1 + ext;
+                break;
+            case 2: //South
+                y -= 1 + ext;
+                break;
+            case 3: //West
+                x -= 1 + ext;
+                break;
+            default: //Not optimal
+                break;
+        }
+        return new Coordinate(x, y);
+    }
+
+    public int mDist(Coordinate c) {
+        return abs(x() - c.x()) + abs(y() - c.y());
+    }
+
+    public int xDist(Coordinate c) {
+        return abs(x() - c.x());
+    }
+
+    public int yDist(Coordinate c) {
+        return abs(y() - c.y());
+    }
 }
