a6f9631c5b2feda724c1835e3ef42d845b2f8a98
atong01 <alexanderytong@gmail.com>
2015-11-28 22:05:23 -0500

    update to coordinate comments

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -29,13 +29,12 @@ public class Coordinate {
      * Calculates a new coordinate relative to an input coordinate
      * Note that coordinate system is x pos is right and y pos is up
      *
-     * @param curr      The current coordinate in (x,y) representation
      * @param dir       The relative direction of the new point relative to the
      *                  current point note that dir is in the set {0,1,2,3},
      *                  where 0=North, 1=East, 2=South, 3=West.
      * @param ext       The distance between the new point and the current point
      *                  i.e. ext(ension) of 0 is adjacent
-     * @return          The new absolute coordinate
+     * @return          The new absolute coordinate, returns null if invalid dir
      */
     public Coordinate calcRelativeLoc(int dir, int ext) {
         int x = x();
@@ -53,14 +52,17 @@ public class Coordinate {
             case 3: //West
                 x -= 1 + ext;
                 break;
-            default: //Not optimal
-                break;
+            default:
+                return null;
         }
         return new Coordinate(x, y);
     }
 
+    /**
+     * Calculates Manhattan distance to another coordinate
+     */
     public int mDist(Coordinate c) {
-        return Math.abs(x() - c.x()) + Math.abs(y() - c.y());
+        return xDist(c) + yDist(c);
     }
 
     public int xDist(Coordinate c) {
