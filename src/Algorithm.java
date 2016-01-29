a46d3a3f5fb99f14ebe77d07a7c110d04e368f18
atong01 <alexanderytong@gmail.com>
2016-01-29 10:35:47 -0500

    added algorithm abstract class

---
+++ src/Algorithm.java
@@ -0,0 +1,47 @@
+
+package src;
+
+public abstract class Algorithm {
+
+    private final boolean[][] inputRobot;
+    private final boolean[][] outputRobot;
+    private final int maxSteps;
+    private Robot currentState;
+    private int step;
+    private boolean success;
+
+    public Algorithm(boolean[][] in, boolean[][] out, int expanded) {
+        inputRobot = in;
+        outputRobot = out;
+        currentState = new Robot(in, expanded);
+        step = 0;
+        success = false;
+    }
+
+    /**
+     * determineParallelStep builds a queue of all opperations to perform in the
+     * next parallel step.
+     *
+     * The algorithm for this function is as follows:
+     *      determine which "part" of the combing algorithm we are on.
+     */
+    public Set<Operation> determineParallelStep() {
+
+    }
+
+    public void performParallelStep(Set<Operation> stepsToPerform) {
+
+    }
+
+    public void run() {
+        for (int i = 0; i < maxSteps; i++) {
+            Set<Operation> steps = determineParallelStep();
+            if (steps.isEmpty()) {
+                success = true;
+                break;
+            }
+            performParallelStep(steps);
+            step++;
+        }
+    }
+}
