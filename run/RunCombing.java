bdec26bebfdd25194553c483a3adaa2744ece629
thekingh <ksingh.pa@gmail.com>
2016-04-18 12:59:31 -0400

    merge viz with master

--- run/RunCombing.java
+++ run/RunCombing.java
@@ -5,9 +5,26 @@ import rutils.*;
 import rgraph.*;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+/*String inputStartPath = "../data/combing/start.json";*/
+/*String inputEndPath   = "../data/combing/end.json";*/
+
 public class RunCombing {
 
     public static List<State> combTest(int[][] start, int[][] finish) {
+
+/*        int[][] start_json = JSONToInt(inputStartPath);*/
+/*        int[][] end_json   = JSONToInt(inputEndPath);*/
+        
         Robot s = BotBuilder.makeBot(start);
         Robot t = BotBuilder.makeBot(finish);
 
@@ -35,4 +52,30 @@ public class RunCombing {
     public static void main(String[] args) {
         BotBuilder.outputStates("../data/combing/", easyCombToLine());
     }
+
+/*    public static int[][] JSONToInt(String path) {*/
+/*        */
+/*        ArrayList<Coordinates> coordinates = new ArrayList<Coordinate>();*/
+/*        JSONParser parser = new JSONParser();*/
+/**/
+/*        try {*/
+/*            Object obj = parser.parse(new FileReader(path));*/
+/*            JSONArray ja = (JSONArray)obj;*/
+/**/
+/*            for(JSONObject jobj : ja) {*/
+/*                int x = (int)jobj.get("x");*/
+/*                int y = (int)jobj.get("y");*/
+/**/
+/*                Coordinate c = new Coordinate(x, y);*/
+/*                coordinates.add(c);*/
+/*            }*/
+/**/
+/*        } catch (FileNotFoundException e) {*/
+/*            e.printStackTrace();*/
+/*        } catch (IOException e) {*/
+/*            e.printStackTrace();*/
+/*        } catch (ParseException e) {*/
+/*            e.printStackTrace();*/
+/*        }*/
+/*    }*/
 }
