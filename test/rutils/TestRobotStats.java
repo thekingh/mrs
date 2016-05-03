f46f18eeeaa33aaabe34a4514a8cc3603ceb2414
atong01 <alexanderytong@gmail.com>
2016-05-03 13:48:36 -0400

    Updates to testRobotStats comments

--- test/rutils/TestRobotStats.java
+++ test/rutils/TestRobotStats.java
@@ -2,6 +2,20 @@ package rutils;
 
 import rutils.*;
 import rgraph.*;
+/**
+ * The Test Robot Stats class prints out helpful statistics about a given Robot
+ * Object. 
+ * <p>
+ * In testing robots we found it helpful to find a number of pieces of information
+ * about a robot in consecuative steps. This class helps to find useful pieces
+ * of information about a robot and print them to the command line for debugging
+ * purposes.
+ *
+ * <p> 
+ * All functions are provided as public for debugging purposes, this class is meant
+ * to be very extensible in allowing for additional functions to be added when
+ * statistics are printed. 
+ */
 public class TestRobotStats {
     public static void printRobotStats(Robot r) {
         System.out.println(RobotStats.getAll(r));
@@ -26,7 +40,6 @@ public class TestRobotStats {
         String ans = stats.getModuleEdgeStat();
         System.out.println(ans);
     }
-
 
     public static void testIsNotExpanded() {
         int[][] start  = {{1,1,0},
